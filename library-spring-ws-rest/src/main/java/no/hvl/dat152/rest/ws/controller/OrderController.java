fcac43f906ab5257b7b440d605e8b928239fd9f6
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2025-10-09 15:30:10 +0200

    OrderController: finito

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/controller/OrderController.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/controller/OrderController.java
@@ -3,17 +3,9 @@
  */
 package no.hvl.dat152.rest.ws.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,11 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.hvl.dat152.rest.ws.exceptions.NoOrdersFoundException;
 import no.hvl.dat152.rest.ws.exceptions.OrderNotFoundException;
-import no.hvl.dat152.rest.ws.exceptions.UserNotFoundException;
 import no.hvl.dat152.rest.ws.model.Order;
 import no.hvl.dat152.rest.ws.service.OrderService;
 
@@ -37,12 +28,31 @@ import no.hvl.dat152.rest.ws.service.OrderService;
 @RequestMapping("/elibrary/api/v1")
 public class OrderController {
 
-	// TODO - getAllBorrowOrders (@Mappings, URI=/orders, and method) + filter by expiry and paginate 
-	
-	// TODO - getBorrowOrder (@Mappings, URI=/orders/{id}, and method)
-	
-	// TODO - updateOrder (@Mappings, URI=/orders/{id}, and method)
-	
-	// TODO - deleteBookOrder (@Mappings, URI=/orders/{id}, and method)
-	
+	@Autowired
+	private OrderService orderService;
+
+	@GetMapping("/orders")
+	public ResponseEntity<List<Order>> getAllBorrowOrders() throws NoOrdersFoundException {
+		List<Order> orders = orderService.findAllOrders();
+		return new ResponseEntity<>(orders, HttpStatus.OK);
+	}
+
+	@GetMapping("/orders/{id}")
+	public ResponseEntity<Order> getBorrowOrder(@PathVariable Long id) throws OrderNotFoundException {
+		Order order = orderService.findOrder(id);
+		return new ResponseEntity<>(order, HttpStatus.OK);
+	}
+
+	@PutMapping("/orders/{id}")
+	public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order order)
+			throws OrderNotFoundException {
+		Order updatedOrder = orderService.updateOrder(order, id);
+		return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/orders/{id}")
+	public ResponseEntity<Void> deleteBookOrder(@PathVariable Long id) throws OrderNotFoundException {
+		orderService.deleteOrder(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
