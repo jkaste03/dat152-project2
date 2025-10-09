aa8f07d2fffb2c7074d9634accd6256ba60b7665
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2025-10-09 13:50:09 +0200

    OrderService: deleteOrder. +NoORdersFoundException

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/OrderService.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/OrderService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import no.hvl.dat152.rest.ws.exceptions.OrderNotFoundException;
+import no.hvl.dat152.rest.ws.exceptions.NoOrdersFoundException;
 import no.hvl.dat152.rest.ws.model.Order;
 import no.hvl.dat152.rest.ws.repository.OrderRepository;
 
@@ -23,28 +24,41 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	public Order saveOrder(Order order) {
-		
+
 		order = orderRepository.save(order);
-		
+
 		return order;
 	}
-	
+
 	public Order findOrder(Long id) throws OrderNotFoundException {
-		
+
 		Order order = orderRepository.findById(id)
-				.orElseThrow(()-> new OrderNotFoundException("Order with id: "+id+" not found in the order list!"));
-		
+				.orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found in the order list!"));
+
 		return order;
 	}
-	
-	// TODO public void deleteOrder(Long id)
-	
-	// TODO public List<Order> findAllOrders()
-	
+
+	public void deleteOrder(Long id) throws OrderNotFoundException {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found in the order list!"));
+		orderRepository.delete(order);
+	}
+
+	public List<Order> findAllOrders() throws NoOrdersFoundException {
+		List<Order> allOrders = orderRepository.findAll();
+
+		if (allOrders.isEmpty()) {
+			throw new NoOrdersFoundException("Could not find any orders in the order list.");
+		}
+
+		return allOrders;
+	}
+
+
 	// TODO public List<Order> findByExpiryDate(LocalDate expiry, Pageable page)
-	
+
 	// TODO public Order updateOrder(Order order, Long id)
 
 }
