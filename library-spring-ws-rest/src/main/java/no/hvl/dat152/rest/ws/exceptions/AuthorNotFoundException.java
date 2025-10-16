22f81a64b17d32a5d35976d54bebd6efda863f8e
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2025-10-16 14:26:42 +0200

    All *NotFoundException: add status code 405 as a tag

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/exceptions/AuthorNotFoundException.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/exceptions/AuthorNotFoundException.java
@@ -2,10 +2,13 @@
  * 
  */
 package no.hvl.dat152.rest.ws.exceptions;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 /**
  * 
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AuthorNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
