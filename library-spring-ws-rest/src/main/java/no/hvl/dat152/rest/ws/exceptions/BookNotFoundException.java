0496cad69b5b3c917c6ac9b3cae48cd9e0e26e4e
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2025-10-16 14:21:46 +0200

    BookNotFoundException:add ResponseStatus tag to return Not Found when called

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/exceptions/BookNotFoundException.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/exceptions/BookNotFoundException.java
@@ -3,9 +3,13 @@
  */
 package no.hvl.dat152.rest.ws.exceptions;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
 /**
  * 
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
