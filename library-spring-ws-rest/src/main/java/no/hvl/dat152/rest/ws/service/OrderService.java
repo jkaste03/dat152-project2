085330bc6cf57da31ff8d7555e20de6a417b1e5d
Vilhelm <52938064+vvilhelmsen@users.noreply.github.com>
2025-10-17 11:01:23 +0200

    OrderService: done

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/OrderService.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/OrderService.java
@@ -23,28 +23,65 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	public Order saveOrder(Order order) {
-		
-		order = orderRepository.save(order);
-		
-		return order;
-	}
-	
-	public Order findOrder(Long id) throws OrderNotFoundException {
-		
-		Order order = orderRepository.findById(id)
-				.orElseThrow(()-> new OrderNotFoundException("Order with id: "+id+" not found in the order list!"));
-		
-		return order;
-	}
-	
-	// TODO public void deleteOrder(Long id)
-	
-	// TODO public List<Order> findAllOrders()
-	
-	// TODO public List<Order> findByExpiryDate(LocalDate expiry, Pageable page)
-	
-	// TODO public Order updateOrder(Order order, Long id)
 
+	public Order saveOrder(Order order) {
+
+		order = orderRepository.save(order);
+
+		return order;
+	}
+
+	public Order findOrder(Long id) throws OrderNotFoundException {
+
+		Order order = orderRepository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found in the order list!"));
+
+		return order;
+	}
+
+	// DONE public void deleteOrder(Long id)
+	public void deleteOrder(Long id) throws OrderNotFoundException {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found in the order list!"));
+
+		orderRepository.delete(order);
+		;
+	}
+
+	// DONE public List<Order> findAllOrders()
+	public List<Order> findAllOrders() throws OrderNotFoundException {
+		if (orderRepository.findAll().isEmpty()) {
+			throw new OrderNotFoundException("Found no orders!");
+		}
+		return orderRepository.findAll();
+	}
+
+	// DONE public List<Order> findByExpiryDate(LocalDate expiry, Pageable page)
+
+	public List<Order> findByExpiryDate(LocalDate expiry, Pageable pageable) throws OrderNotFoundException {
+		Page<Order> result;
+
+		if (expiry == null) {
+			result = orderRepository.findAll(pageable);
+		} else {
+			result = orderRepository.findByExpiryBefore(expiry, pageable);
+		}
+
+		if (result.isEmpty()) {
+			throw new OrderNotFoundException("No orders found for the given expiry date!");
+		}
+
+		return result.getContent();
+	}
+
+	// DONE public Order updateOrder(Order order, Long id)
+	public Order updateOrder(Order order, Long id) throws OrderNotFoundException {
+		Order eOrder = orderRepository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found in the order list!"));
+		eOrder.setIsbn(order.getIsbn());
+		eOrder.setExpiry(order.getExpiry());
+		orderRepository.save(eOrder);
+
+		return eOrder;
+	}
 }
